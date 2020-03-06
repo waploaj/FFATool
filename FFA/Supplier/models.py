882880e5fb60e20ccfa10bfcf4695b67270ec0d5
Abdallah Abdillah <waploaj@Abdallahs-MacBook-Pro.local>
2020-03-06 23:06:39 +0300

    create suppliers models

--- FFA/Supplier/models.py
+++ FFA/Supplier/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from Person.models import OsposPeople
 
 # Create your models here.
+class OsposSuppliers(OsposPeople):
+    person = models.ForeignKey(OsposPeople, models.DO_NOTHING)
+    company_name = models.CharField(max_length=255)
+    agency_name = models.CharField(max_length=255)
+    account_number = models.CharField(unique=True, max_length=255, blank=True, null=True)
+    deleted = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'ospos_suppliers'
